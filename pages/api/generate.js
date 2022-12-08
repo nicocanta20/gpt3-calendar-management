de244ee8daf514e2c5daa47669c57015d222ae02
nicocanta20 <nicocanta20@gmail.com>
2022-12-08 11:23:52 -0300

    new interface and functionalities

---
+++ pages/api/generate.js
@@ -0,0 +1,97 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+const generateAction = async (req, res) => {
+  const PromptPrefix = 
+  `
+  make a reminder in google tasks for:
+  ${req.body.userInput}
+
+  if it applies, also give some tips on the description on how to make this task the best way possible. make a three bullet ideas
+  the format should be:
+  Task: 
+  Time:(if its not specified should return "N/A")
+  Date:(if its not specified should return "N/A")
+  Location:(if its not specified should return "N/A")
+  Description: 
+`
+  console.log(`API: ${PromptPrefix}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${PromptPrefix}`,
+    temperature: 0.7,
+    max_tokens: 250,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
+
+// prompt chaining
+// const generateAction = async (req, res) => {
+
+// const basePromptPrefix =
+// `
+// make a reminder in google tasks for:
+// ${req.body.userInput}
+
+// if it applies, also give some tips on the description on how to make this task the best way possible. make a three bullet ideas
+// the format should be:
+// Task: 
+// Time:(if its not specified should return "N/A")
+// Date:(if its not specified should return "N/A")
+// Location:(if its not specified should return "N/A")
+// Description: 
+// `
+//   console.log(`API: ${basePromptPrefix}`)
+
+//   const baseCompletion = await openai.createCompletion({
+//     model: 'text-davinci-003',
+//     prompt: `${basePromptPrefix}`,
+//     temperature: 0.8,
+//     max_tokens: 250,
+//   });
+  
+//   const basePromptOutput = baseCompletion.data.choices.pop();
+
+//   // I build Prompt #2.
+
+//   const secondPrompt =
+//   `
+//   make a reminder in google tasks for:
+//   ${req.body.userInput}
+
+//   calendar event example: ${basePromptOutput.text}
+
+//   calendar event:
+// `
+
+  
+//   // I call the OpenAI API a second time with Prompt #2
+//   const secondPromptCompletion = await openai.createCompletion({
+//     model: 'text-davinci-003',
+//     prompt: `${secondPrompt}`,
+//     // I set a higher temperature for this one. Up to you!
+//     temperature: 0.85,
+// 		// I also increase max_tokens.
+//     max_tokens: 1250,
+//   });
+  
+//   // Get the output
+//   const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+//   // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+//   res.status(200).json({ output: secondPromptOutput });
+// };
+
+// export default generateAction;
