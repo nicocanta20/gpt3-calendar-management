b77bb98d19b4ff626b2b6bb8dc601ba8f0fc4145
nicocanta20 <nicocanta20@gmail.com>
2023-01-14 15:56:30 -0300

    update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -31,6 +31,36 @@ const generateAction = async (req, res) => {
   
   const basePromptOutput = baseCompletion.data.choices.pop();
 
+  function dateIsValid(date) {
+    return date instanceof Date && !isNaN(date);
+  }
+
+  let date=basePromptOutput.text.split("Date:")[1].split("Location:")[0].trim()
+  if(date=="Tomorrow"){
+    date=new Date()
+    date.setDate(date.getDate()+1)
+    date=date.toDateString()
+    basePromptOutput.text=basePromptOutput.text.split("Date:")[0]+"Date: "+date+basePromptOutput.text.split("Tomorrow")[1]
+
+  }
+  else{
+    const dateCompletion = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `transform ${date} to this type: month-date-2023`,
+      temperature: 0.7,
+      max_tokens: 300,
+    });
+    
+    const dataPromptOutput = dateCompletion.data.choices.pop();
+    //remove \n from the string
+    dataPromptOutput.text=dataPromptOutput.text.replace(/(\r\n|\n|\r)/gm, "")
+    if (dateIsValid(new Date(dataPromptOutput.text))){
+      basePromptOutput.text=basePromptOutput.text.split("Date:")[0]+"Date: "+dataPromptOutput.text+"\n"+"Location:"+basePromptOutput.text.split("Location:")[1]
+    }
+    else{
+      basePromptOutput.text=basePromptOutput.text.split("Date:")[0]+"Date: N/A\n"+"Location:"+basePromptOutput.text.split("Location:")[1]
+    }
+  }
   res.status(200).json({ output: basePromptOutput });
 };
 
--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ const Home = () => {
 )}
 </div>
       </div>
-      <div className="badge-container grow">
+      {/* <div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
           target="_blank"
@@ -87,7 +87,7 @@ const Home = () => {
             <p>build with buildspace</p>
           </div>
         </a>
-      </div>
+      </div> */}
     </div>
   );
 };
